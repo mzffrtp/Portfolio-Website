f897ddd4207a00e293d81579cd2871583f7890f1
mzffrtp <mzffr.tpgl@gmail.com>
2023-04-29 19:55:07 +0200

    project page routing and styling

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-export default function Projects () {
+export default function Projects() {
+    const isDarkMode = useSelector(state => state.themeState.darkMode)
     return (
-        <div>
-            projects
+        <div className={`${isDarkMode ? "bg-primary-light" : "bg-primary-dark text-white"} min-h-screen relative z-10 px-3 overflow-hidden`}>
+            <section className="h-full max-w-5xl pt-4 mx-aoto mb-16 md:pt-16">
+                <Link to={"/"}>
+                    <div className="flex items-center space-x-4 font-semibold pb-6 text-gray-600 hover:text-red-900">
+                        ⬅️ <span className="ms-3">Back to Home</span>
+                    </div>
+                </Link>
+                <div className="grid grid-cols-1 gap-6 md:gap-8 md:grid-cols-2 border-2 border-white">
+
+                </div>
+
+            </section>
         </div>
     )
 }
